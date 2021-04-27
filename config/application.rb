5c336dc7da4a291c93768b590999e2c622c97027
Ernest O. Fokuo <kwesifokuo@gmail.com>
2021-04-27 09:16:07 +0000

    Updated config/application.rb re-edit

--- config/application.rb
+++ config/application.rb
@@ -6,12 +6,11 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-config.assets.initialize_on_precompile = false
-
 module InvestmentApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    config.assets.initialize_on_precompile = false
 
     # Configuration for the application, engines, and railties goes here.
     #
